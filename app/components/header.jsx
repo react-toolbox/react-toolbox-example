3b8bd605782fa59195b412a45baf0a4a642423bc
Javi Velasco <javier.velasco86@gmail.com>
2015-11-15 20:43:56 +0100

    Use react-toolbox 0.12.1 and add ToolboxApp wrapper

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import style from './style';
-
-// require components separated to get in the build
-// just what you are using
 import AppBar from 'react-toolbox/lib/app_bar';
 import Button from 'react-toolbox/lib/button';
+import style from './style';
 
-const MainAppBar = (props) => {
-  return (
-    <AppBar className={style.appbar} flat>
-      <h1 className={style.title}>React Toolbox Example</h1>
-      <Button className={style.button} kind="floating" icon="add" accent/>
-    </AppBar>
-  );
-};
+const MainAppBar = () => (
+  <AppBar className={style.appbar} flat>
+    <h1 className={style.title}>React Toolbox Example</h1>
+    <Button className={style.button} kind="floating" icon="add" accent/>
+  </AppBar>
+);
 
 export default MainAppBar;
