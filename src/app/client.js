b363173bbb0656ad57d1c77d9519cff64fc01037
stereobooster <stereobooster@gmail.com>
2016-10-04 17:29:44 +0300

    Switching to custom-react-scripts
    Trying styles get to work

--- src/app/client.js
+++
@@ -1,6 +0,0 @@
-import 'react-toolbox/lib/commons.scss';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.js';
-
-ReactDOM.render(<App />, document.getElementById('app'));
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './index.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));
